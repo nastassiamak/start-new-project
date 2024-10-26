a5b0828230425dd6cb85fa7fee221e1ea6e3d97c
«nastassiamak» <redfox.asya@gmail.com>
2024-10-26 10:01:59 +0300

    add vercel.json

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express, {NextFunction, Request, Response} from 'express'
+import bodyParser from 'body-parser'
+import {productsRouter} from "./routes/products-router";
+import {addressesRouter} from "./routes/addresses-router";
+
+const app = express()
+const port = process.env.PORT || 3000
+
+
+
+const parserMiddleware = bodyParser({})
+app.use(parserMiddleware)
+
+app.use('/products', productsRouter)
+app.use('/addresses', addressesRouter)
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
